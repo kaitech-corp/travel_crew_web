1bde00e694243b4d364212016b941ff69fb9caf3
randynolden <randy@kaitechcorp.com>
2022-01-22 15:38:32 -0800

    Added Firebase

--- animations.js
+++
@@ -1,8 +0,0 @@
-
-const elmnt = document.getElementById('div-features');
-const container1 = document.getElementById("container-1");
-// document.getElementById("download_button").onclick = scrollToFeatures();
-// document.getElementById("download_button").onclick = function(){
-//   document.querySelector('.button-primary').style.color = 'darkorange';
-//
-// }
